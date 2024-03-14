aea0824ace579a74056c05f78668aa5118b5ec8e
TimonPC\Timon <gtreet>
2024-03-14 22:37:16 +0100

    requirements and gunicorn added

--- Users/serializers.py
+++ Users/serializers.py
@@ -12,15 +12,3 @@ class CustomUserSerializer(serializers.ModelSerializer):
         user = CustomUser.objects.create_user(**validated_data)
         password = validated_data.get('password')
         return user
-
-
-class CreateUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = 'id', 'username', 'email', 'role', "first_name", "last_name", "password"
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
-        password = validated_data.get('password')
-        return user
