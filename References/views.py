413421aaca38474c3f0bdf885b9a91b6097d5f6a
Timon Laptop <bedynek@themarmalade.com>
2024-03-26 17:30:20 +0100

    filter recursiv added

--- References/views.py
+++ References/views.py
@@ -4,8 +4,18 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
+import django_filters
 
+class ReferenceFilter(FilterSet):
+    game__name = django_filters.CharFilter(field_name='game__name', lookup_expr='icontains')
+    game__game_category__name = django_filters.CharFilter(field_name='game__game_category__name', lookup_expr='icontains')
+    game__release_date = django_filters.DateFilter(field_name='game__release_date')
+    game__developer = django_filters.CharFilter(field_name='game__developer', lookup_expr='icontains')
+
+    class Meta:
+        model = Reference
+        fields = ['game__name', 'game__game_category__name', 'game__release_date', 'game__developer']
 
 class GameCategoryViewSet(viewsets.ModelViewSet):
     queryset = GameCategory.objects.all()
@@ -28,7 +38,8 @@ class ReferenceViewSet(viewsets.ModelViewSet):
     queryset = Reference.objects.all()
     permission_classes = [AllowAny]
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['game']
+    filterset_class = ReferenceFilter
+
 
     # @method_decorator(cache_page(60))
     # def dispatch(cls, *args, **kwargs):
